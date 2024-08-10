8123b1b39aa564e5e6a4ead8413a8678d122b1c6
NFhook <NFhook>
2024-08-10 23:01:19 +0800

    update syn client

--- syn_client.h
+++ syn_client.h
@@ -1,4 +1,11 @@
 #ifndef SYN_CLIENT_H
 #define SYN_CLIENT_H
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
 #endif

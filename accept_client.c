8c684d10f0c11ba9270098103ccbef222299bc7f
NFhook <NFhook>
2024-08-10 23:00:48 +0800

    update accept client

--- accept_client.c
+++ accept_client.c
@@ -1,8 +1,34 @@
 #include "accept_client.h"
 
 
-int main(int argc, char *argv[])
-{
-    printf("ACCEPT QUEUE CLIENT EXAMPLE\n");
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <IP> <PORT>\n", argv[0]);
+        return -1;
+    }
+
+    char *ip = argv[1];
+    int port = atoi(argv[2]);
+
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        return -1;
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    inet_pton(AF_INET, ip, &server_addr.sin_addr);
+
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("connect");
+        close(sock);
+        return -1;
+    }
+
+    printf("Connected to server\n");
+    close(sock);
     return 0;
 }

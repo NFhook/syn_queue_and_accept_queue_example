9beb239158c2d6bed10e32effbe8960871681b0c
NFhook <NFhook>
2024-08-10 23:01:45 +0800

    update syn server

--- syn_server.h
+++ syn_server.h
@@ -1,4 +1,12 @@
 #ifndef SYN_SERVER_H
 #define SYN_SERVER_H
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+
 #endif
